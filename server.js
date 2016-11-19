47b87c6f8474456c4f8f0ba88c73f787278e6e38
Ulises Gascón <ulisesgascongonzalez@gmail.com>
2016-11-19 21:41:36 +0000

    Refactor and optimization

--- server.js
+++ server.js
@@ -1,63 +1,74 @@
-var express = require('express');
-var fileupload = require("express-fileupload");
-var dna = require('dna2json');
-var fs = require('fs');
-var bodyParser = require('body-parser');
-var queries = require("./queries")
-var app = express();
+var express = require('express'),
+    fileupload = require("express-fileupload"),
+    dna = require('dna2json'),
+    fs = require('fs'),
+    bodyParser = require('body-parser'),
+    queries = require("./queries"),
+    pug = require("pug"),
+    app = express();
 
-app.use( bodyParser.json() );
+app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({
   extended: true
-})); 
-
+}));
 app.use(fileupload());
 app.use(express.static(__dirname + '/public'));
 
-app.post("/upload", function(req, res)
-{
-    var file;
+app.set('view engine', 'pug');
+app.set('views', './views');
 
-    if(!req.files)
-    {
-        res.send("File was not found");
-        return;
-    }
-    console.log(req.files)
-    file = req.files.archivo_adn;  // here is the field name of the form
+app.locals.pretty = true; // indent produces HTML for clarity
 
-    file.mv("archivo_adn.txt", function(err) {
-        if(err){
-         console.log("ERRRORRR!!! ")
-         res.send("ERRRRORRRRR!!")
-        } else {
-            
-            var txt = fs.readFileSync('archivo_adn.txt', 'utf8');
-            
-            dna.parse(txt, function(err, snps){
-                if(err){
-                    console.log("ERROR En conversión a SNPS")
-                }
-                fs.writeFile('./archivo_adn.json', JSON.stringify(snps, null, 2) , 'utf-8', function(err){
-                    if (err) return console.log(err);
-                    var finalAdn = require('./archivo_adn.json')
-                                    console.log(req.body)
-                console.log(req.params)
-                var data = {}
-                if(req.params){
-                    if(typeof req.body.norovirus !== "undefined"){
-                        data.norovirus = queries.norovirus(finalAdn);
-                    }
-                    if(typeof req.body.gendermale !== "undefined"){
-                        data.gendermale = queries.gendermale(finalAdn);
-                    }
-                }
-                res.send(data);
-                });
 
-            });
-        }
-    });
+app.get('/', function(req, res) {
+  res.render('index');
 });
+
+app.get("/results", function(req, res) {
+    res.redirect(301, '/');
+});
+
+app.post("/results", function(req, res) {
+
+    if (JSON.stringify(req.body) === '{"action":""}' ) {
+      res.redirect(301, '/');
+      return;
+    } 
+
+  
+  if(req.files.archivo_adn.name !== ""){
+    var file = req.files.archivo_adn;
+    file.mv("archivo_adn.txt", function(err) {
+      if (err) {
+        res.send("ERROR con el archivo enviado.");
+        return;
+      } else {
+        var txt = fs.readFileSync('archivo_adn.txt', 'utf8');
+        dna.parse(txt, function(err, snps) {
+          if (err) {
+            res.send("ERROR En conversión a SNPS");
+            return;
+          } else {
+              console.log("Datos del archivo enviado");
+              askAdn(req, res, snps);
+          }
+        });
+      }
+    });
+  } else {
+      console.log("Datos del archivo demo");
+      askAdn(req, res, require("./demo-adn.json"));
+    }
+});
+
+function askAdn (req, res, adn){
+    var data = [];
+    if (req.body) {
+        for (var item in req.body) {
+          data.push(queries[item](adn));
+        }
+    }
+    res.render("index", {datos: data});
+}
 
 app.listen(process.env.PORT || 3000);
