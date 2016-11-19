54b36e6a9cf3192130603ab8cabf4f87dfc47bd0
Ulises Gascón <ulisesgascongonzalez@gmail.com>
2016-11-19 17:49:17 +0000

    Testing flow

---
+++ queries.js
@@ -0,0 +1,36 @@
+var gql = require('gql');
+
+var queries = {
+    norovirus: function(adn){
+        return adn.rs601338.genotype == 'AA'
+    },
+    gendermale: function(adn){
+        var isMale = gql.atLeast(2, [
+          gql.exact('rs2032651', 'D'),
+          gql.has('rs2032651', 'A'),
+          gql.has('rs9341296', 'C'),
+          gql.has('rs9341296', 'T'),
+          gql.has('rs13304168', 'C'),
+          gql.has('rs13304168', 'T'),
+          gql.has('rs1118473', 'A'),
+          gql.has('rs1118473', 'G'),
+          gql.has('rs150173', 'A'),
+          gql.has('rs150173', 'C'),
+          gql.has('rs16980426', 'G'),
+          gql.has('rs16980426', 'T'),
+          gql.or([
+            gql.exact('rs1558843', 'C'),
+            gql.exact('rs1558843', 'A')
+          ]),
+          gql.or([
+            gql.exact('rs17222419', 'C'),
+            gql.exact('rs17222419', 'T')
+          ])
+        ]);
+        
+        return isMale(adn);
+        
+    }
+}
+
+module.exports = queries;
--- server.js
+++ server.js
@@ -3,11 +3,11 @@ var fileupload = require("express-fileupload");
 var dna = require('dna2json');
 var fs = require('fs');
 var bodyParser = require('body-parser');
+var queries = require("./queries")
 var app = express();
 
-
-app.use( bodyParser.json() );       // to support JSON-encoded bodies
-app.use(bodyParser.urlencoded({     // to support URL-encoded bodies
+app.use( bodyParser.json() );
+app.use(bodyParser.urlencoded({
   extended: true
 })); 
 
@@ -38,16 +38,26 @@ app.post("/upload", function(req, res)
                 if(err){
                     console.log("ERROR En conversión a SNPS")
                 }
-                //fs.writeFile('./archivo_adn.json', JSON.stringify(snps, null, 2) , 'utf-8');
-                console.log(req.body)
+                fs.writeFile('./archivo_adn.json', JSON.stringify(snps, null, 2) , 'utf-8', function(err){
+                    if (err) return console.log(err);
+                    var finalAdn = require('./archivo_adn.json')
+                                    console.log(req.body)
                 console.log(req.params)
-                res.send("ok");    // echo the result back
+                var data = {}
+                if(req.params){
+                    if(typeof req.body.norovirus !== "undefined"){
+                        data.norovirus = queries.norovirus(finalAdn);
+                    }
+                    if(typeof req.body.gendermale !== "undefined"){
+                        data.gendermale = queries.gendermale(finalAdn);
+                    }
+                }
+                res.send(data);
+                });
+
             });
         }
     });
-
-    //res.send("File Uploaded");
-
 });
 
 app.listen(process.env.PORT || 3000);
