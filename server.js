8bbc7582def9d396953aa7fe528365d6e1903110
Ulises Gascón <ulisesgascongonzalez@gmail.com>
2016-11-19 14:18:25 +0000

    Más cambios...

--- server.js
+++ server.js
@@ -1,6 +1,15 @@
 var express = require('express');
 var fileupload = require("express-fileupload");
+var dna = require('dna2json');
+var fs = require('fs');
+var bodyParser = require('body-parser');
 var app = express();
+
+
+app.use( bodyParser.json() );       // to support JSON-encoded bodies
+app.use(bodyParser.urlencoded({     // to support URL-encoded bodies
+  extended: true
+})); 
 
 app.use(fileupload());
 app.use(express.static(__dirname + '/public'));
@@ -19,11 +28,25 @@ app.post("/upload", function(req, res)
 
     file.mv("archivo_adn.txt", function(err) {
         if(err){
-         console.log("ERRRORRR!!! ")   
+         console.log("ERRRORRR!!! ")
+         res.send("ERRRRORRRRR!!")
+        } else {
+            
+            var txt = fs.readFileSync('archivo_adn.txt', 'utf8');
+            
+            dna.parse(txt, function(err, snps){
+                if(err){
+                    console.log("ERROR En conversión a SNPS")
+                }
+                //fs.writeFile('./archivo_adn.json', JSON.stringify(snps, null, 2) , 'utf-8');
+                console.log(req.body)
+                console.log(req.params)
+                res.send("ok");    // echo the result back
+            });
         }
     });
 
-    res.send("File Uploaded");
+    //res.send("File Uploaded");
 
 });
 
