5a838e0d1b26fa50d48bd7c3a5e29db28c4375e9
Ulises Gascón <ulisesgascongonzalez@gmail.com>
2016-11-19 13:27:06 +0000

    Migration to Express and file upload

--- server.js
+++ server.js
@@ -1,16 +1,30 @@
-var project = require('pillars');
+var express = require('express');
+var fileupload = require("express-fileupload");
+var app = express();
 
-project.services.get('http').configure({
-    port: process.env.PORT || 3000
-}).start();
+app.use(fileupload());
+app.use(express.static(__dirname + '/public'));
 
-var staticRoute = new Route({
-    id: 'staticRoute',
-    path: '/*:path',
-    directory: {
-        path: './public',
-        listing: true
+app.post("/upload", function(req, res)
+{
+    var file;
+
+    if(!req.files)
+    {
+        res.send("File was not found");
+        return;
     }
+    console.log(req.files)
+    file = req.files.archivo_adn;  // here is the field name of the form
+
+    file.mv("archivo_adn.txt", function(err) {
+        if(err){
+         console.log("ERRRORRR!!! ")   
+        }
+    });
+
+    res.send("File Uploaded");
+
 });
 
-project.routes.add(staticRoute);
+app.listen(process.env.PORT || 3000);
